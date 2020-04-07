ee5ccb4187aba7ba47d335fdf87831adfc25d4c7
Connor Brabant <connorbrabant@gmail.com>
2020-04-07 12:04:07 -0400

    Signup Form basic CSS

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -40,16 +40,14 @@ class SignUp extends React.Component {
     showForm() {
         return (
             <form className='signup-form' onSubmit={this.handleSubmit} >
-                <label className='signup-child'>Ludicrous Nickname
-                        <input className='signin-child' type='text' onChange={this.update('username')} value={this.state.username} />
-                </label>
-                <label className='signup-child'>Email
-                        <input className='signup-child' type='text' onChange={this.update('email')} value={this.state.emailup} />
-                </label>
-                <label className='signup-child' >Password (I forgot my password)
-                        <input className='signup-child' type='text' onChange={this.update('password')} value={this.state.password} />
-                </label>
-                <button className='signup-child' type='submit'>Login</button>
+                <label htmlFor='username' className='signin-child'>Ludicrous Nickname</label>
+                <input id='username' className='signin-child' type='text' onChange={this.update('username')} value={this.state.username} />
+                <label htmlFor='email' className='signup-child'>Email</label>
+                <input id='email' className='signup-child' type='text' onChange={this.update('email')} value={this.state.email} />
+                <label htmlFor='password' className='signup-child' >Password</label>
+                <input id='password' className='signup-child' type='text' onChange={this.update('password')} value={this.state.password} />
+                <p className='terms-p'>By clicking "Create Account", you are indicating that you have read and agree to the Terms of Service.</p>
+                <button className='signup-button signup-child' type='submit'>Create Account</button>
             </form>
         )
     }
@@ -60,13 +58,13 @@ class SignUp extends React.Component {
     render() {
         const errors = this.props.errors.map((error, i) => <li key={i}>{error}</li>)
         return (
-            <div className='session-forms signin'>
-                <h1 className='signin main'>Sign Up</h1>
-                <h2 className='sign in secondary'>and show off your genius</h2>
-                <button onClick={() => this.state.formPresent ? this.setState({ formPresent: false }) : this.setState({ formPresent: true })}>Sign up with email</button>
+            <div className='session-forms-page signin'>
+                <h1 className='signup-main'>SIGN UP</h1>
+                <h2 className='signup-secondary'>and show off your genius</h2>
+                <button className='button form-button' onClick={() => this.state.formPresent ? this.setState({ formPresent: false }) : this.setState({ formPresent: true })}>Sign up with email</button>
                 <ul>{errors}</ul>
                 {this.state.formPresent ? this.showForm() : null}
-                <p>Don't have an account? Sign up here.</p>
+                <p className='signup-signin'>Already have an account? Sign in here.</p>
             </div>
         )
     }
