ab868bbfc5a7da3d2f25591339850457da68e3fb
Connor Brabant <connorbrabant@gmail.com>
2020-04-06 23:17:17 -0400

    Basic CS of Session Forms and Navbar

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
-import * as Util from './util/session'
 import Root from './components/root'
 
 document.addEventListener('DOMContentLoaded', () => {
-    
-    //Testing 
-    // window.store = store;
-    // window.createUser = Util.postUser;
-    // window.createSession = Util.postSession;
-    // window.deleteSession = Util.deleteSession;
-    // Testing
     
     let store;
     if (window.currentUser) {
